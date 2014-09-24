c799b0894c4f11f481f9ee5163e9f9aba1d2a7e6
Victor <vicwu97@gmail.com>
2014-09-24 10:51:42 -0400

    Nothing

--- str.c
+++ str.c
@@ -10,13 +10,10 @@ char * mystr(char *, char *);
 
 int main() {
 
-  char a[] = "Helln";
-  char b[] = "Hellno";
+  char a[64] = "Imagine";
+  char b[] = "Dragons";
 
-  //printf("mylen 7 = %d\n", mylen(a));
-  //printf("%c\n", *mycpy(a,b));
-
-  printf("%d\n", mycmp(a,b));
+  printf("%s\n", mycat(a,b));
 
   return 0;
 }
@@ -38,17 +35,34 @@ int mycmp(char *a, char *b) {
       return -1;
     i++;
   }
-  
-  return 0;
+
+  if (*a == 0 && *b == 0)
+    return 0;
+  else if (*a == 0)
+    return -1;
+  else
+    return 1;
 }
 
 char * mycpy(char *d, char *s) {
   int i = 0;
 
-  for(i = 0; i < mylen(s); i++) {
+  for(i = 0; i < mylen(s); i++)
     d[i] = s[i];
-  }
 
   return d;
 }
 
+char * mycat(char *d, char *s) {
+  int i = 0;
+
+  while (s[i]) {
+    d[mylen(d) + i] = s[i];
+    printf("Adding: %c\n", s[i]);
+    printf("%c\n", d[mylen(d) + i]);
+    i++;
+    printf("--Added: %s\n", d);
+  }
+
+  return d;
+}
