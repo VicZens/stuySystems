ff27f1069619a75d7113a6337720f3f022db942a
Victor <vicwu97@gmail.com>
2014-09-30 10:51:24 -0400

    Free

--- mem.c
+++ mem.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
   double *d;
@@ -8,8 +9,15 @@ int main() {
   d = (double *)malloc(10 * sizeof(double));  
   c = (char *)malloc(20 * sizeof(char));
 
-  d[40] = 0;
-  c[20] = 0;
+  d[0] = 7.17;
+  c[0] = 0;
+  strcpy(c, "wuhoo");
+
+  printf("%lf\n", d[0]);
+  printf("%s\n", c);
+
+  free(d);
+  free(c);
 
   return 0;
 }
