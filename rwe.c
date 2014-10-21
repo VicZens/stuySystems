5a54581688af0a4fb9241a2130691882884ac550
Victor <vicwu97@gmail.com>
2014-10-21 11:06:46 -0400

    Read Write Execute

---
+++ rwe.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+
+typedef struct intring {
+  int i;
+  char s[256];
+} intring;
+
+int main() {
+  
+
+  return 0;
+}
