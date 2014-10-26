684b370c61bcb45b07956675e057b707d5dce11b
Victor Wu <vicwu97@gmail.com>
2014-10-26 13:44:01 -0400

    Curr

--- direct.c
+++ direct.c
@@ -9,8 +9,32 @@ int main() {
   getcwd(curr_path, sizeof(curr_path));
   printf("Current Working Directory: %s \n", curr_path);
 
-  //while (entry = readdir(d)) {  
-  //}
+  DIR *d;
+  d = opendir(curr_path);
 
+  struct dirent* entry;
+  printf("Directories:\n");
+  while (entry = readdir(d)) {
+    if (entry->d_type==4) {
+      printf("\t %s \n", entry->d_name);
+    }
+  }
+
+  printf("\n");
+  rewinddir(d);
+
+  int size;
+  struct stat *for_size;
+  stat(curr_path, for_size);
+  printf("Regular Files:\n");
+  while (entry = readdir(d)) {
+    if (entry->d_type==8) {
+      size = size + (*for_size).st_size;
+      printf("\t %s \n", entry->d_name);
+    }
+  }
+
+  closedir(d);
+  
   return 0;
 }
