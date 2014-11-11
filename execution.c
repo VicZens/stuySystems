501c5d0b220651cfea6cbaa24e81ad0a25f8adf4
Victor Wu <vicwu97@gmail.com>
2014-11-11 14:21:29 -0500

    Done

--- execution.c
+++ execution.c
@@ -6,19 +6,27 @@
 int main() {
   char* input;
   char* exec;
+  char* arg;
+
   input = (char*)malloc(255*sizeof(char));
+  exec = (char*)malloc(255*sizeof(char));
+  arg = (char*)malloc(255*sizeof(char));
 
   //Reads in input, and parses it to the command
   fgets(input, sizeof(input), stdin);
-  exec = strsep(&input, "\n");
-
-  //Need to parse the rest of the string
+  arg = strsep(&input, "\n");
+  exec = strsep(&arg, " ");
 
   //Running the program
-  execlp(exec, "-a", NULL);
+  execlp(exec, " ", arg, NULL);
+  
+  //^^^ I don't get why, but once the " " is taken out, it doesn't work as
+  //intended. Since this works, which was achieved through random testing,
+  //I'm going to leave it be.
 
   //Testing for execution of program
   printf("%s", exec);
+  printf("%s", arg);
 
   return 0;
 }
