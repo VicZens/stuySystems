e98437e1713a5152fbc25142b98ba28871140c39
Victor Wu <vicwu97@gmail.com>
2014-11-11 10:18:55 -0500

    Woo

--- execution.c
+++ execution.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main() {
-  char exe[255];
-  fgets(exe, sizeof(exe), stdin);
+  char* input;
+  char* exec;
+  input = (char*)malloc(255*sizeof(char));
 
-  printf("%s", exe);
+  //Reads in input, and parses it to the command
+  fgets(input, sizeof(input), stdin);
+  exec = strsep(&input, "\n");
+
+  //Need to parse the rest of the string
+
+  //Running the program
+  execlp(exec, "-a", NULL);
+
+  //Testing for execution of program
+  printf("%s", exec);
 
   return 0;
 }
