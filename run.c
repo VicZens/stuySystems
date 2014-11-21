c5be8f1d1d06733331a674bb916bc05123c94261
Victor Wu <vicwu97@gmail.com>
2014-11-21 11:01:49 -0500

    Push

--- run.c
+++ run.c
@@ -2,10 +2,26 @@
 
 //Testing Time!!!
 int main() {
-
-  //Function to count the number of spaces in a string
+  parse_command("How to make this work");
 
   //exec_command("ls", "-al");
 
   return 0;
 }
+
+
+//Function to count the number of spaces in a string
+  /*
+  char test[256] = "How many spaces are there";
+  char c = ' ';
+  int i;
+  int count; 
+
+  while(test[i]) {
+    if(test[i] == c)
+      count++;
+    i++;
+  }
+
+  printf("%d\n", count);
+  */
--- shell.c
+++ shell.c
@@ -19,15 +19,30 @@ int exec_command(char* run, char* arg) {
 
 char** parse_command(char* input) {
   char** output;
-  char* new_input;
-  char* temp;
   output = (char**)malloc(256*sizeof(char));
+  char* new_input;
   new_input = (char*)malloc(256*sizeof(char));
+  char* temp;
   temp = (char*)malloc(256*sizeof(char));
+  int i;
   
   new_input = strsep(&input, "\n");
-  
+  printf("%s\n", new_input);
 
+  printf("1\n");
+  while(new_input) {
+    printf("2\n");
+    strsep(&new_input, " ");
+    printf("3\n");
+    strcpy(output[i], temp);
+    i++;
+  }
+  
+  i = 0;
+  //while(output[i]) {
+  //strcpy(output[0], "Hello");
+  //printf("%s\n", output[0]);
+  //i++; }
   return output;
 }
 
