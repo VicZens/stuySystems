3fa3a36e91fd1014e30bea99ba16be87ec5a9d93
Victor Wu <vicwu97@gmail.com>
2014-11-30 17:44:37 -0500

    Final Changes

--- run.c
+++ run.c
@@ -4,7 +4,7 @@ int main() {
   char curr_dir[256];
   char* input = (char*)malloc(256*sizeof(char));
   printf("Initiating...\n");
-  sleep(1.5);
+  sleep(1);
   exec_command(parse_command("clear"));
   printf("Welcome to Bash!\n(Please read the manual)\n\n");
   
@@ -15,5 +15,9 @@ int main() {
     check_and_run(input);
   }
 
+  printf("Bye...");
+  sleep(1);
+  //exec_command(parse_command("clear"));
+  
   return 0;
 }
--- shell.c
+++ shell.c
@@ -27,6 +27,54 @@ char** parse_command(char* input) {
   return output;
 }
 
+int exec_command(char** args) {
+  int f, status;
+  f = fork();
+
+  if (f==0)
+    execvp(args[0], args);
+  else 
+    wait(&status);
+
+  return status;
+}
+
+int check_and_run(char* input) {
+  char** cmd;
+  
+  if(!check_command(input)) {
+    cmd = parse_command(input);
+    exec_command(cmd);
+  }
+  return 0;
+}
+
+int redirect(char* source, char* dest) {
+  int fd_source, fd_dest;
+  char temp[256] = "touch ";
+  strcat(temp, dest);
+  check_and_run(temp);
+  fd_dest = open(dest, O_WRONLY | O_TRUNC);
+  fd_source = dup(STDOUT_FILENO);
+  dup2(fd_dest, STDOUT_FILENO);
+  check_and_run(source);
+  dup2(fd_source, STDOUT_FILENO);
+
+  return 0;
+}
+
+char* remove_newline(char* input) {
+  int i = 0;
+
+  while(input[i]) {
+    if(!strcmp(&input[i], "\n"))
+      input[i] = '\0';
+    i++;
+  }
+
+  return input;
+}
+
 int check_command(char* input) {
   int ret_val = 0;
   char** cmd;
@@ -54,68 +102,33 @@ int check_command(char* input) {
     input = remove_newline(input);
     redirect(input, temp);
     ret_val++;
-  } else if(strstr(input, "|")) {
+  } 
+  /*
+    else if(strstr(input, "|")) {
     temp = strsep(&input, "|");
     input = remove_newline(input);
     piping(temp, input);
     ret_val++;
-  } 
-  /*
-  else if(strstr(input, ">>")) {
+    } 
+    else if(strstr(input, ">>")) {
     printf("Found it");
     temp = strsep(&input, ">>");
     input = remove_newline(input);
     redirect_append(temp, input);
     ret_val++;
-  } else if(strstr(input, "<<")) {
+    } else if(strstr(input, "<<")) {
     temp = strsep(&input, "<<");
     input = remove_newline(input);
     redirect_append(input, temp);
     ret_val++;
-  }
+    }
   */
 
   return ret_val;
 }
 
-
-int exec_command(char** args) {
-  int f, status;
-  f = fork();
-
-  if (f==0)
-    execvp(args[0], args);
-  else 
-    wait(&status);
-
-  return 0;
-}
-
-int check_and_run(char* input) {
-  char** cmd;
-  
-  if(!check_command(input)) {
-    cmd = parse_command(input);
-    exec_command(cmd);
-  }
-  return 0;
-}
-
-int redirect(char* source, char* dest) {
-  int fd_source, fd_dest;
-  char temp[256] = "touch ";
-  strcat(temp, dest);
-  check_and_run(temp);
-  fd_dest = open(dest, O_WRONLY | O_TRUNC);
-  fd_source = dup(STDOUT_FILENO);
-  dup2(fd_dest, STDOUT_FILENO);
-  check_and_run(source);
-  dup2(fd_source, STDOUT_FILENO);
-
-  return 0;
-}
-
-int redirect_append(char* source, char* dest) {
+/*
+  int redirect_append(char* source, char* dest) {
   int fd_source, fd_dest;
   //char temp[256] = "touch ";
   //strcat(temp, dest);
@@ -127,21 +140,16 @@ int redirect_append(char* source, char* dest) {
   dup2(fd_source, STDOUT_FILENO);
 
   return 0;
-}
-
-int piping(char* source, char* dest) {
-  
-  return 0;
-}
-
-char* remove_newline(char* input) {
-  int i = 0;
-
-  while(input[i]) {
-    if(!strcmp(&input[i], "\n"))
-      input[i] = '\0';
-    i++;
   }
+*/
 
-  return input;
-}
+/*
+  int piping(char* source, char* dest) {
+  
+  char string[SIZE];
+  freopen("/dev/null", "a", stdout);
+  setbuf(stdout, string);
+
+  return 0;
+  }
+*/
